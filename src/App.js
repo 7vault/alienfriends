dae7ef23a3232466c917fbb0246aac77c9bc891f
7vault <wnestand.dev@gmail.com>
2023-01-02 11:43:33 -0800

    action to the search added, it can filter the cardlist by name

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import { aliens } from "./aliens";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>AlienFriends</h1>
-      <SearchBox />
-      <CardList aliens={aliens} />
-    </div>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      aliens,
+      searchField: "",
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    const filteredAliens = this.state.aliens.filter((aliens) => {
+      return aliens.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
+
+    return (
+      <div className="tc">
+        <h1>AlienFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList aliens={filteredAliens} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const SearchBox = () => {
-  return <input type="search" placeholder="search contact name"></input>;
+const SearchBox = ({ searchField, searchChange }) => {
+  return (
+    <div>
+      <input
+        className="pa3 br3 bw2 b--green"
+        type="search"
+        placeholder="search contact name"
+        onChange={searchChange}
+      ></input>
+    </div>
+  );
 };
 
 export default SearchBox;
