db905aa025631d310a939b6260ab1999c95d7da9
7vault <wnestand.dev@gmail.com>
2023-01-02 13:33:28 -0800

    adding scroll view and loading animation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
-import { aliens } from "./aliens";
+// import { aliens } from "./aliens"; for local data ressources
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
+import "./app.css";
+import Scroll from "./Scroll";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      aliens,
+      aliens: [],
       searchField: "",
     };
+  }
+
+  componentDidMount() {
+    // pulling data from ressources online
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ aliens: users });
+      });
   }
 
   onSearchChange = (event) => {
@@ -23,13 +36,19 @@ class App extends Component {
         .includes(this.state.searchField.toLowerCase());
     });
 
-    return (
-      <div className="tc">
-        <h1>AlienFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList aliens={filteredAliens} />
-      </div>
-    );
+    if (this.state.aliens.length === 0) {
+      return <h1 className="txtName tc f1">LOADIND...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">AlienFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList aliens={filteredAliens} />
+          </Scroll>
+        </div>
+      );
+    }
   }
 }
 
--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./card.css";
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className=" tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-8 ">
+    <div className=" tc dib br3 pa3 ma2 grow bw2 shadow-8 card-container">
       <img alt="aliens" src={`https://robohash.org/${id}/?set=set2`} />
       <div>
-        <h2>{name}</h2>
-        <p>{email}</p>
+        <h2 className="txtName">{name}</h2>
+        <p className="txtEmail">{email}</p>
       </div>
     </div>
   );
